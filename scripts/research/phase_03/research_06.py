67ac9409b58ddef070dbb3eb3d633b7754c23479
Jerry Kiely <jerry@cowboysmall.com>
2024-05-25 20:54:39 +0100

    added research script - using scikit-learn LogisticRegression

---
+++ scripts/research/phase_03/research_06.py
@@ -0,0 +1,213 @@
+
+"""
+
+Global market indices of interest:
+
+    NSEI:  Nifty 50
+    DJI:   Dow Jones Index
+    IXIC:  Nasdaq
+    HSI:   Hang Seng
+    N225:  Nikkei 225
+    GDAXI: Dax
+    VIX:   Volatility Index
+
+"""
+
+
+
+# %% 1 - import required libraries
+import pandas as pd
+import numpy as np
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, roc_curve, roc_auc_score
+from sklearn.model_selection import train_test_split
+
+from cowboysmall.data.file import read_master_file
+from cowboysmall.feature.indicators import get_indicators, INDICATORS
+from cowboysmall.plots import plt, sns
+
+
+
+# %% 2 -
+INDICES  = ['NSEI', 'DJI', 'IXIC', 'HSI', 'N225', 'GDAXI', 'VIX']
+COLUMNS  = [f"{index}_DAILY_RETURNS" for index in INDICES]
+RATIOS   = ["NSEI_HL_RATIO", "DJI_HL_RATIO"]
+
+ALL_COLS = COLUMNS + RATIOS + INDICATORS
+
+FEATURES = ["IXIC_DAILY_RETURNS", "HSI_DAILY_RETURNS", "N225_DAILY_RETURNS", "VIX_DAILY_RETURNS", "NSEI_RSI"]
+
+
+# %% 2 -
+master = read_master_file()
+
+
+
+# %% 2 -
+master["NSEI_OPEN_DIR"] = np.where(master["NSEI_OPEN"] > master["NSEI_CLOSE"].shift(), 1, 0)
+
+
+
+# %% 2 -
+master["NSEI_HL_RATIO"] = master["NSEI_HIGH"] / master["NSEI_LOW"]
+master["DJI_HL_RATIO"]  = master["DJI_HIGH"] / master["DJI_LOW"]
+
+
+
+# %% 2 -
+master = get_indicators(master)
+
+
+
+
+# %% 2 -
+counts = master['NSEI_OPEN_DIR'].value_counts().reset_index()
+counts.columns = ['NSEI_OPEN_DIR', 'Freq']
+print(counts)
+#    NSEI_OPEN_DIR  Freq
+# 0              1  1064
+# 1              0   499
+
+
+
+# %% 2 -
+print((counts["Freq"][0] / (counts["Freq"][0] + counts["Freq"][1])).round(3))
+# 0.681
+
+
+
+# %% 3 -
+data = pd.concat([master["NSEI_OPEN_DIR"].shift(-1), master[ALL_COLS]], axis = 1)
+data.dropna(inplace = True)
+data.head()
+
+
+
+# %% 3 -
+X = data.loc[:, FEATURES]
+y = data.loc[:, "NSEI_OPEN_DIR"]
+
+
+
+# %% 4 -
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 1337)
+
+
+
+# %% 5 -
+model = LogisticRegression(max_iter = 1000)
+model.fit(X_train, y_train)
+
+
+
+# %% 8 - 
+y_predprob = model.predict_proba(X_train)
+
+fpr, tpr, thresholds = roc_curve(y_train, y_predprob[:, 1])
+
+plt.plot_setup()
+sns.sns_setup()
+plt.roc_curve(fpr, tpr, "06_01", "01 - training data", "phase_03")
+
+
+
+# %% 9 - Optimal Threshold
+optimal_threshold = round(thresholds[np.argmax(tpr - fpr)], 3)
+print(f'Best Threshold is : {optimal_threshold}')
+# Best Threshold is : 0.678
+
+
+
+# %% 10 - AUC Curve
+auc_roc = roc_auc_score(y_train, y_predprob[:, 1])
+print(f'AUC ROC: {auc_roc}')
+# AUC ROC: 0.7493606138107417
+
+
+
+# %% 11 - Classification Report
+X_train['predicted_class'] = np.where(y_predprob[:, 1] <= optimal_threshold,  0, 1)
+print(classification_report(y_train, X_train['predicted_class']))
+#               precision    recall  f1-score   support
+# 
+#          0.0       0.53      0.66      0.59       391
+#          1.0       0.82      0.73      0.77       829
+# 
+#     accuracy                           0.71      1220
+#    macro avg       0.68      0.69      0.68      1220
+# weighted avg       0.73      0.71      0.71      1220
+
+
+
+# %% 11 - 
+table = pd.crosstab(X_train['predicted_class'], y_train)
+table
+# NSEI_OPEN_DIR    0.0  1.0
+# predicted_class          
+# 0                257  224
+# 1                134  605
+
+
+
+# %% 11 - 
+sensitivity = round((table.iloc[1, 1] / (table.iloc[0, 1] + table.iloc[1, 1])) * 100, 2)
+print(f"Sensitivity for cut-off {optimal_threshold} is : {sensitivity}%")
+# Sensitivity for cut-off 0.63 is : 72.98%
+
+specificity = round((table.iloc[0, 0] / (table.iloc[0, 0] + table.iloc[1, 0])) * 100, 2)
+print(f"Specificity for cut-off {optimal_threshold} is : {specificity}%")
+# Specificity for cut-off 0.63 is : 65.73%
+
+
+
+# %% 12 - ROC Curve
+y_predprob = model.predict_proba(X_test)
+
+fpr, tpr, thresholds = roc_curve(y_test, y_predprob[:, 1])
+
+plt.plot_setup()
+sns.sns_setup()
+plt.roc_curve(fpr, tpr, "06_02", "02 - testing data", "phase_03")
+
+
+
+# %% 13 - AUC Curve
+auc_roc = roc_auc_score(y_test, y_predprob[:, 1])
+print(f'AUC ROC: {auc_roc}')
+# AUC ROC: 0.7592684377478192
+
+
+
+# %% 14 - Classification Report
+X_test['predicted_class'] = np.where(y_predprob[:, 1] <= optimal_threshold,  0, 1)
+print(classification_report(y_test, X_test['predicted_class']))
+#               precision    recall  f1-score   support
+# 
+#          0.0       0.54      0.67      0.60        97
+#          1.0       0.83      0.73      0.78       208
+# 
+#     accuracy                           0.71       305
+#    macro avg       0.68      0.70      0.69       305
+# weighted avg       0.73      0.71      0.72       305
+
+
+
+# %% 11 - 
+table = pd.crosstab(X_test['predicted_class'], y_test)
+table
+# NSEI_OPEN_DIR    0.0  1.0
+# predicted_class          
+# 0                 65   56
+# 1                 32  152
+
+
+
+# %% 11 - 
+sensitivity = round((table.iloc[1, 1] / (table.iloc[0, 1] + table.iloc[1, 1])) * 100, 2)
+print(f"Sensitivity for cut-off {optimal_threshold} is : {sensitivity}%")
+# Sensitivity for cut-off 0.63 is : 73.08%
+
+specificity = round((table.iloc[0, 0] / (table.iloc[0, 0] + table.iloc[1, 0])) * 100, 2)
+print(f"Specificity for cut-off {optimal_threshold} is : {specificity}%")
+# Specificity for cut-off 0.63 is : 67.01%
