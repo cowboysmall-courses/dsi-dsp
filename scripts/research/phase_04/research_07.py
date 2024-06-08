96e41411ce1acdfee887377fbd63ed0b27f0a7b3
Jerry Kiely <jerry@cowboysmall.com>
2024-06-08 22:06:18 +0100

    phase 4 - added a deep learning classifier

--- scripts/research/phase_04/research_07.py
+++ scripts/research/phase_04/research_07.py
@@ -18,16 +18,14 @@ Global market indices of interest:
 # %% 1 - import required libraries
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plot
 
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, roc_curve, roc_auc_score
-from sklearn.linear_model import LogisticRegression
-from sklearn.naive_bayes import GaussianNB
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.svm import SVC
-from sklearn.neural_network import MLPClassifier
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
+
+import torch
+import torch.nn as nn
 
 from cowboysmall.data.file import read_master_file
 from cowboysmall.feature.indicators import get_indicators, INDICATORS
@@ -43,6 +41,42 @@ RATIOS   = ["NSEI_HL_RATIO", "DJI_HL_RATIO"]
 ALL_COLS = COLUMNS + RATIOS + INDICATORS
 
 FEATURES = ["IXIC_DAILY_RETURNS", "HSI_DAILY_RETURNS", "N225_DAILY_RETURNS", "VIX_DAILY_RETURNS", "DJI_RSI", "DJI_TSI"]
+
+
+
+# %% 2 -
+class MLP(nn.Module):
+
+    def __init__(self, input_dim, output_dim):
+        super(MLP, self).__init__()
+
+        self.input  = nn.Linear(input_dim, 16)
+        self.act_in = nn.ReLU()
+
+        self.hidden1 = nn.Linear(16, 16)
+        self.act1 = nn.ReLU()
+        self.hidden2 = nn.Linear(16, 16)
+        self.act2 = nn.ReLU()
+        self.hidden3 = nn.Linear(16, 16)
+        self.act3 = nn.ReLU()
+        self.hidden4 = nn.Linear(16, 16)
+        self.act4 = nn.ReLU()
+        self.hidden5 = nn.Linear(16, 16)
+        self.act5 = nn.ReLU()
+
+        self.output  = nn.Linear(16, output_dim)
+        self.act_out = nn.Sigmoid()
+
+    def forward(self, x):
+        x = self.act_in(self.input(x))
+
+        x = self.act1(self.hidden1(x))
+        x = self.act2(self.hidden2(x))
+        x = self.act3(self.hidden3(x))
+        x = self.act4(self.hidden4(x))
+        x = self.act5(self.hidden5(x))
+
+        return self.act_out(self.output(x))
 
 
 
@@ -75,8 +109,11 @@ data.head()
 
 
 # %% 3 -
-X = data[ALL_COLS]
-y = data['NSEI_OPEN_DIR']
+scaler = MinMaxScaler(feature_range = (0, 1))
+
+X = scaler.fit_transform(data[ALL_COLS].values) 
+y = data['NSEI_OPEN_DIR'].values[:, None]
+
 
 
 
@@ -84,28 +121,61 @@ y = data['NSEI_OPEN_DIR']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 1337)
 
 
+# %% 1 -
+X_train = torch.from_numpy(X_train).type(torch.Tensor)
+X_test  = torch.from_numpy(X_test).type(torch.Tensor)
+y_train = torch.from_numpy(y_train).type(torch.Tensor)
+
+
 
 # %% 1 -
-model  = MLPClassifier(hidden_layer_sizes = (100, 100, 100), max_iter = 1000, alpha = 0.00001, random_state = 1337)
+input_dim  = X.shape[1]
+output_dim = 1
 
-scaler = MinMaxScaler()
-# scaler = StandardScaler()
+model     = MLP(input_dim, output_dim)
+criterion = nn.MSELoss()
+optimiser = torch.optim.Adam(model.parameters(), lr = 0.01)
 
-model.fit(scaler.fit_transform(X_train), y_train)
+
+
+# %% 1 -
+epochs = 500
+losses = []
+
+for epoch in range(epochs):
+    out  = model(X_train)
+    loss = criterion(out, y_train)
+
+    losses.append(loss.item())
+
+    optimiser.zero_grad()
+    loss.backward()
+    optimiser.step()
+
+    if epoch % 10 == 9:
+        print(f"Epoch {epoch + 1:>3} - MSE: {loss.item()}")
+
+
+
+# %% 1 -
+plot.figure(figsize = (15, 6))
+plot.plot(losses, label = "Loss")
+plot.legend()
+plot.show()
+
+
+
+# %% 1 -
+y_pred = model(X_test).detach().cpu().numpy()
 
 
 
 # %% 6 - ROC Curve
-y_pred = model.predict_proba(scaler.transform(X_test))
-
-
-
-# %% 6 -
-fpr, tpr, thresholds = roc_curve(y_test, y_pred[:, 1])
+fpr, tpr, thresholds = roc_curve(y_test, y_pred)
 
 plt.plot_setup()
 sns.sns_setup()
-plt.roc_curve(fpr, tpr, "06_01", "Neural Network", "phase_04")
+plt.roc_curve(fpr, tpr, "07_01", "Neural Network", "phase_04")
 
 
 
@@ -117,14 +187,14 @@ print(f'Best Threshold is : {optimal_threshold}')
 
 
 # %% 8 - AUC Curve
-auc_roc = roc_auc_score(y_test, y_pred[:, 1])
+auc_roc = roc_auc_score(y_test, y_pred)
 print(f'AUC ROC: {auc_roc}')
 # AUC ROC: 0.7532216494845361
 
 
 
 # %% 10 - Classification Report
-y_pred_class = np.where(y_pred[:, 1] <= optimal_threshold,  0, 1)
+y_pred_class = np.where(y_pred <= optimal_threshold,  0, 1)
 print(classification_report(y_test, y_pred_class))
 #               precision    recall  f1-score   support
 # 
@@ -138,8 +208,8 @@ print(classification_report(y_test, y_pred_class))
 
 
 # %% 11 - 
-table = pd.crosstab(y_pred_class, y_test)
-table
+table = pd.crosstab(y_pred_class[:, 0], y_test[:, 0])
+print(table)
 # NSEI_OPEN_DIR  0.0  1.0
 # row_0                  
 # 0               54   28
